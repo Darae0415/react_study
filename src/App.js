c9d1bb3f4e88b7fd414d4af0cf55cb9d896ecd86
Darae0415 <ekfogksmf@naver.com>
2024-07-26 12:17:23 +0900

    7. Delect

--- src/App.js
+++ src/App.js
@@ -20,8 +20,10 @@ function Nav(props) {
     let t = props.topics[i];
     lis.push(<li key={t.id}>
         <a id={t.id} href={'/read' + t.id} onClick={(e) => {
+        // 링크를 클릭하면 '/read'와 id가 합쳐진 주소로 이동
             e.preventDefault();
-            props.onChangeMode(Number(e.target.id));  // id를 Number로 변환하여 전달
+            props.onChangeMode(Number(e.target.id));  
+            // 클릭한 항목의 id를 숫자로 변환하여 onChangeMode 함수에 전달해요.
           }}>{t.title}</a>
       </li>)
   }
@@ -67,7 +69,10 @@ function Create(props){
   );
 }
 function Update(props){
+  // Update = Create + Read
+  // props는 사용자가 component로 전달한 명령어(조선시대 말로 component에게 어명)
   const [title, setTitle] = useState(props.title);
+  //props.title을 useState로 환승해서 setTitle로 움겨서 title에 저장
   const [body, setBody] = useState(props.body);
   return(
     <article>
@@ -77,11 +82,13 @@ function Update(props){
         const title = e.target.title.value;
         const body = e.target.body.value;
         props.onUpdate(title,body);
+        //on Update가 다른곳에도 설정되어야 됨
       }}>
         <p><input type='text' name='title' placeholder='title' value={title} onChange={(e)=>{
           setTitle(e.target.value);
+          // e(이벤트된).target(대상의).value(값);
         }}/></p>
-        {/* react에서는 onChange가 값이 바뀔때마다 Change가 됨 */}
+        {/* react에서는 onChange가 값이 바뀔때마다(입력할때마다) Change가 됨 */}
         <p><textarea name='body' placeholder='body' value={body} onChange={(e)=>{
           setBody(e.target.value);
         }}/></p>
@@ -121,10 +128,27 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
-    contextControl = <li><a href={'/update/'+id} onClick={(e)=>{
+    contextControl = <>
+    <li><a href={'/update/'+id} onClick={(e)=>{
+      // a링크를 클릭했을때 /update/라는 곳에 이동하고 +id는 변수인데,
+      // '/update/' + id(5로 설정 되있다는 가정하에) 라 했을때 => 
+      //update의 id 5번 웹으로 이동하게 하는 것. 
       e.preventDefault();
       setMode('Update')
     }}>Update</a></li>
+      <li><input type='button' value={"Delect"} onClick={()=>{
+        const newTopics = []
+        for(let i=0; i<topics.length; i++){
+          if(topics[i].id !== id){   
+          // !==는 같은거의 부정이므로 같지 않을때
+            newTopics.push(topics[i]);
+          }
+        }
+        setTopics(newTopics);
+        setMode('Welcome');
+      }}>
+      </input></li>
+    </>
     // contextControl을 mode가 Read 일때만 보이게 함
   } else if(mode === 'Create'){
     content = <Create onCreate={(_title,_body)=>{
@@ -158,19 +182,30 @@ function App() {
       if(topics[i].id === id){
         title = topics[i].title;
         body = topics[i].body;
+        // mode가 Update다 변수title,body를 null(빈통)로 만들어주고
+        // for문으로 i는0이고 i가 topics의 length보다 작을때 i++증감연산 해줌
+        // 만약(if) topics[i]번 id가 설정한 id값이랑 같다면
+        // title에 = topics[i]의 타이틀 값, body= topics[i]의 바디값을 넣어줌
       }
     }
     content = <Update title={title} body={body} onUpdate={(title, body)=>{
+      //title,body가 변경된 값을 topics에 저장하기 위한 식
       const updatedTopic = {id:id, title:title, body:body}
       const newTopics = [...topics]
+      // 기존의 주제들(topics)을 새로운 배열(newTopics)로 복사
       for(let i=0; i<newTopics.length; i++){
+      // 모든 주제를 확인하기 위해 반복문을 사용
         if(newTopics[i].id === id){
+        // 현재 주제의 id가 변경된 주제의 id와 같으면
           newTopics[i] = updatedTopic;
-          break;
+          // 그 주제를 변경된 내용으로 바꿔줌
+          break; //그리고 반복문 멈춤
         }
       }
       setTopics(newTopics);
+      // 새로운 주제 목록(newTopics)을 setTopics에 저장
       setMode('Read');
+      // 화면 모드를 'Read'(읽기) 모드로 바꿈
     }}></Update>
   }
 
@@ -193,6 +228,19 @@ function App() {
           setMode('Create'); //클릭하면 mode를(setMode) Create로 바꿈
         }}>create</a></li>
         {contextControl}
+        {/*
+        contextControl => <>
+        <li><a href={'/update/'+id} onClick={(e)=>{ 
+          e.preventDefault();
+          setMode('Update')}}>Update</a></li>
+          <li><input type='button' value={"Delect"} onClick={()=>{
+            const newTopics = []
+            for(let i=0; i<topics.length; i++){
+              if(topics[i].id !== id){newTopics.push(topics[i]);}}
+            setTopics(newTopics);
+            setMode('Welcome');}}>
+          </input></li> 
+              */}
       </ul>
     </div>
   );
